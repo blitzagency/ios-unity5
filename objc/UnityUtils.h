f3b2a6ec201d5bd1b83e655a0347b8b2eed1fef0
Armin Willer <armin_willer@yahoo.de>
2017-07-11 19:13:14 +0200

    Prevent linker error

--- objc/UnityUtils.h
+++ objc/UnityUtils.h
@@ -7,7 +7,12 @@
 #ifndef UnityUtils_h
 #define UnityUtils_h
 
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 void custom_unity_init(int argc, char* argv[]);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* UnityUtils_h */
